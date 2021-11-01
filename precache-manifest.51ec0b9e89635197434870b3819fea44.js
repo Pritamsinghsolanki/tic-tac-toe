2df0238fe429ff7f6494a8bbe3f5a8255846b7ec
Pritamsinghsolanki <pritamsignhsolanki2000@gmail.com>
2021-11-02 01:34:26 +0530

    Updates

---
+++ precache-manifest.51ec0b9e89635197434870b3819fea44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77859bfc6bab5693a5e4275609b5a020",
+    "url": "/Pritamsinghsolanki/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "0df285a610856ff52880",
+    "url": "/Pritamsinghsolanki/tic-tac-toe/static/css/main.c9656128.chunk.css"
+  },
+  {
+    "revision": "e1c6a816ccf14fa6bf7d",
+    "url": "/Pritamsinghsolanki/tic-tac-toe/static/js/2.9ead5ae3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Pritamsinghsolanki/tic-tac-toe/static/js/2.9ead5ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df285a610856ff52880",
+    "url": "/Pritamsinghsolanki/tic-tac-toe/static/js/main.193556a1.chunk.js"
+  },
+  {
+    "revision": "324bd725559effb9c4d9",
+    "url": "/Pritamsinghsolanki/tic-tac-toe/static/js/runtime-main.321fc324.js"
+  }
+]);
